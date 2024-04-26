aadbeac2124f0dffd013560226e54e3bd6725850
A-UKD <118949035+A-UKD@users.noreply.github.com>
2024-04-26 05:37:08 +0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -32,6 +32,10 @@ while True:
         show_person()
     elif choice == 2:
         book = input("Введіть назву книги: ")
+        if book in person["books_statistics"]:
+            print("В читача є книга з таким іменем")
+        else:
+            person["books_statistics"].append(book)
         person["books_in_use"].append(book)
     elif choice == 3:
         print("Книги в використанні:", ", ".join(person["books_in_use"]))
